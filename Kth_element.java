e7da8ac5262c0a43ca3c0bd4f293413c60ea6420
Shreya8937 <99526081+Shreya8937@users.noreply.github.com>
2024-05-20 09:10:02 -0400

    Update Kth_element.java

--- Kth_element.java
+++ Kth_element.java
@@ -10,13 +10,13 @@ public class Kth_element{
         System.out.println("Original Array: ");
         System.out.println(Arrays.toString(arr));
 
-        System.out.println("K'th smallest element of the said array: ");
+        System.out.println("K'th smallest element of the array is: ");
        
         Arrays.sort(arr);
       
         System.out.print(arr[k-1] + " ");
      
-        System.out.println("\nK'th largest element of the said array:");
+        System.out.println("\nK'th largest element of the array is:");
         
     
         System.out.print(arr[k-1] + " ");
