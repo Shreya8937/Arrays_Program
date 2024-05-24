280d4fd6bade9d9ba519ef71c961fb17990a2d01
Shreya8937 <99526081+Shreya8937@users.noreply.github.com>
2024-05-24 14:29:18 +0530

    Update Array_initialization.java

--- Array_initialization.java
+++ Array_initialization.java
@@ -1,6 +1,6 @@
  class Array_initialization {
 		 public static void main(String[] args) {
-		        int[] array = {1, 2, 3, 4, 5};
+		        int[] array = {1, 2, 3, 4, 5,6,7};
 		      
 		        System.out.println("Array created with elements: " + java.util.Arrays.toString(array));
 		    }
