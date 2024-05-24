249c55e617e925d49afbe94fb6cf252bcb25dffa
Shreya8937 <99526081+Shreya8937@users.noreply.github.com>
2024-05-24 14:40:53 +0530

    Update Looping_Of_Array.java

--- Looping_Of_Array.java
+++ Looping_Of_Array.java
@@ -1,6 +1,6 @@
 public class Looping_Of_Array {
 	  public static void main(String[] args) {
-	        int[] array = {1, 2, 3, 4, 5};
+	        int[] array = {1, 2, 3, 4, 5,6};
 	        
 	        for (int i = 0; i <= array.length; i++) {
 	            System.out.println("Element at index " + i + ": " + array[i]);
